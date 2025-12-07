f4fca42ff31d63ecd286130e2f933541dfcf0387
Manuel Tamayo Montero <manueltamayo9765@gmail.com>
2025-12-06 21:20:50 -0600

    obtiene las datos de estadiscas

---
+++ POSNet.Application/DTOs/ReportStatisticsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSNet.Application.DTOs
+{
+    public class ReportStatisticsDTO
+    {
+
+        public int Ventas { get; set; }
+        public decimal Ingresos { get; set; }
+        public int UnidadesTotales { get; set; }
+        public int ValorInventario { get; set; }
+    }
+}
---
+++ POSNet.Application/Features/Reports/Queries/Handlers/GetStatisticsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using POSNet.Application.DTOs;
+using POSNet.Application.Interfaces;
+
+namespace POSNet.Application.Features.Reports.Queries.Handlers
+{
+    public class GetStatisticsQueryHandler : IRequestHandler<GetStatisticsQuery, ReportStatisticsDTO>
+    {
+        private readonly IVentasRepository ventasRepository;
+        private readonly IProductsRepository productsRepository;
+
+        public GetStatisticsQueryHandler(IVentasRepository ventasRepository, IProductsRepository productsRepository)
+        {
+            this.ventasRepository = ventasRepository;
+            this.productsRepository = productsRepository;
+        }
+        public async Task<ReportStatisticsDTO> Handle(GetStatisticsQuery request, CancellationToken cancellationToken)
+        {
+
+            var ReportStatistics = new ReportStatisticsDTO()
+            {
+                Ventas = await ventasRepository.getTotalVentas(),
+                Ingresos = await ventasRepository.getTotalRevenue(),
+                UnidadesTotales = await productsRepository.getTotalUnitsProducts(),
+                ValorInventario = await productsRepository.getTotalInventory()
+            };
+
+            return ReportStatistics;
+        }
+    }
+}
